ec0e453b9d166834966819b444ffde2604640601
LupusUmbrae <robin_moss@btinternet.com>
2014-11-06 23:19:38 +0000

    Version upload added
    Initial box uploading supporting added

--- vagrantPyCloud.py
+++ vagrantPyCloud.py
@@ -2,12 +2,15 @@ import json
 import os
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, Response, send_from_directory
 from werkzeug import secure_filename
+import hashlib
 
 
 DEBUG = True
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 BOX_ROOT = os.path.join(APP_ROOT, 'boxes')
+
+SERVER_NAME = "localhost:5000"
 
 BOX_EXTENSION = "box";
 
@@ -73,69 +76,132 @@ def craeteBox():
 
 @app.route('/upload/uploaded/', methods=['POST'])
 def upload():
-  print "hello"
-  boxName = request.form['box']
-  print boxName
-  if request.form['upload'] == "box":
-    print "upload box"
-    
-    file = request.files['boxFile']
-    version = request.form['version']
-    box = request.form['box']
-    
-    boxHome = os.path.join(app.config['BOX_ROOT'], box)
-    boxVersion = os.path.join(boxHome, version)
-    
-    print "Making dirs"
-    
-    os.makedirs(boxVersion)
-    print boxVersion
-    
-    if file and allowed_file(file.filename):
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(boxVersion, filename))
-  elif request.form['upload'] == "create":
-    print "create box"
-  return render_template('uploaded.html')
+      print "Upload"
+      title = ""
+      message = "Hi?"
+      
+      if request.form['upload'] == "box":
+            title = "upload box"
+            
+            file = request.files['boxFile']
+            filename = secure_filename(file.filename)
+            version = request.form['version']
+            box = request.form['box']
+            provider = request.form['provider']
+            metadata = json.loads(getBoxMetadataFile(box).read())
+            
+            
+            if versionLegal(version, metadata, provider):
+            
+                  boxHome = os.path.join(app.config['BOX_ROOT'], box)
+                  boxVersion = os.path.join(boxHome, version)
+                                    
+                  os.makedirs(boxVersion)
+                  print boxVersion
+                  
+                  if file and allowed_file(filename):                  
+                        filePath = os.path.join(boxVersion, filename)
+                        file.save(filePath)
+                        newVersion = processFile(filePath, filename, provider, box, version)
+                        print newVersion
+                        addOrUpdateVersion(metadata, newVersion)
+                        print metadata
+                        saveBoxMetadata(box, metadata)
+                  else:
+                    message = "Illegal file"
+            else:
+                message = "Version/Provider already in exists"
+            
+            message = "Sucessfully uploaded version :" + version + " to box: " + box + " for provider: " + provider
+                    
+      elif request.form['upload'] == "create":
+            print "create box"
+            
+      return render_template('uploaded.html', title=title, message=message)
 
 @app.route('/upload/version', methods=['POST'])
 def addVersion():
-  box = request.form['box']
-  return render_template('add_version.html', boxName=box)
+    print app.config['SERVER_NAME']
+    box = request.form['box']
+    return render_template('add_version.html', boxName=box)
 
 @app.route('/upload/provider', methods=['POST'])
 def addProvider():
-  return render_template('new_provider.html')
+    return render_template('new_provider.html')
 
 
 #
 # Utilities
 #
 
-def getBoxMetadataFile(box):
-  boxHome = os.path.join(app.config['BOX_ROOT'], box)
-  if os.path.isdir(boxHome):
-    metadataFile = os.path.join(boxHome, 'metadata.json')
-    f = open(metadataFile, 'r')
-    return f
+def getBoxMetadataFile(box, mode='r'):
+    boxHome = os.path.join(app.config['BOX_ROOT'], box)
+    if os.path.isdir(boxHome):
+        metadataFile = os.path.join(boxHome, 'metadata.json')
+        f = open(metadataFile, mode)
+        return f
+
+def saveBoxMetadata(box, metadata):
+    f = getBoxMetadataFile(box, 'w')
+    data = json.dumps(metadata, sort_keys=True, indent=4, separators=(',', ': '))
+    f.write(data)
+    f.close()
+    
        
 def getBoxes():
-  boxes=[]
-  for folder in os.listdir(app.config['BOX_ROOT']):
-    boxes.append(folder)
-  return boxes
+    boxes=[]
+    for folder in os.listdir(app.config['BOX_ROOT']):
+        boxes.append(folder)
+    return boxes
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] == BOX_EXTENSION
           
 
-def versionLegal(version, metadata):
-  for curVersion in metadata["versions"]:
-    if curVersion["version"] == version:
-      return false
-  return true
-  
+def versionLegal(version, metadata, provider):
+    for curVersion in metadata["versions"]:
+        if curVersion["version"] == version:
+            for curProvider in curVersion['providers']:
+                if curProvider == provider:
+                    return False
+            return False
+    return True
+
+def processFile(filepath, filename, provider, box, version):
+    print "process file"
+    sha1 = hashlib.sha1()
+    f = open(filepath, 'rb')
+    try:
+    
+        sha1.update(f.read())
+
+        
+
+        url = "http://" +app.config['SERVER_NAME'] + "/boxes/"  + box + "/" + version + "/" +  filename
+
+        print url
+
+        providerDetails = {}
+        providerDetails["name"] = provider
+        providerDetails["url"] = url
+        providerDetails["checksum_type"] = "sha1"
+        providerDetails["checksum"] = sha1.hexdigest()
+
+        newVersion = {'version':version, 'providers':[providerDetails]}
+        
+        return newVersion
+      
+    finally:
+        f.close()
+
+def addOrUpdateVersion(metadata, versionJson):
+    for curVersion in metadata["versions"]:
+        if curVersion["version"] == versionJson["version"]:
+            curVersion["providers"].extend(versionJson["providers"])
+            return
+    metadata["versions"].append(versionJson)
+
 
 if __name__ == "__main__":
   app.run()
